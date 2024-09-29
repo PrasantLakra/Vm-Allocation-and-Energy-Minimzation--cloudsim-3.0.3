13adc17be6de4063382021551f83d103645322ab
PrasantLakra <125748405+PrasantLakra@users.noreply.github.com>
2024-09-29 21:40:49 +0530

    Add files via upload

---
+++ RoundRobin/RandomRunner.java
@@ -0,0 +1,82 @@
+package RoundRobin;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.examples.power.Helper;
+import org.cloudbus.cloudsim.examples.power.RunnerAbstract;
+import org.cloudbus.cloudsim.examples.power.random.RandomHelper;
+
+public class RandomRunner extends RunnerAbstract {
+
+	/**
+	 * @param enableOutput
+	 * @param outputToFile
+	 * @param inputFolder
+	 * @param outputFolder
+	 * @param workload
+	 * @param vmAllocationPolicy
+	 * @param vmSelectionPolicy
+	 * @param parameter
+	 */
+	public RandomRunner(
+			boolean enableOutput,
+			boolean outputToFile,
+			String inputFolder,
+			String outputFolder,
+			String workload,
+			String vmAllocationPolicy,
+			String vmSelectionPolicy,
+			String parameter) {
+		super(
+				enableOutput,
+				outputToFile,
+				inputFolder,
+				outputFolder,
+				workload,
+				vmAllocationPolicy,
+				vmSelectionPolicy,
+				parameter);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.cloudbus.cloudsim.examples.power.RunnerAbstract#init(java.lang.String)
+	 */
+	@Override
+	protected void init(String inputFolder) {
+		try {
+			CloudSim.init(1, Calendar.getInstance(), false);
+
+			broker = Helper.createBroker();
+			int brokerId = broker.getId();
+
+			 Scanner scan = new Scanner(System.in);
+			 System.out.println("Enter number of HOSTS:- ");
+		        int NUMBER_OF_HOSTS = scan.nextInt();
+				System.out.println("Enter number of VMS:- ");
+			       int NUMBER_OF_VMS = scan.nextInt();
+			      
+			        
+			        cloudletList = RandomHelper.createCloudletList(brokerId, NUMBER_OF_VMS);
+				vmList = Helper.createVmList(brokerId, cloudletList.size());
+				
+				 
+				hostList = Helper.createHostList(NUMBER_OF_HOSTS);
+		/*		
+			cloudletList = RandomHelper.createCloudletList(brokerId, RandomConstants.NUMBER_OF_VMS);
+			vmList = Helper.createVmList(brokerId, cloudletList.size());
+			hostList = Helper.createHostList(RandomConstants.NUMBER_OF_HOSTS);
+			*/
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			Log.printLine("The simulation has been terminated due to an unexpected error");
+			System.exit(0);
+		}
+	}
+
+}
