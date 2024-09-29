556ac20224a91d93eb0ca12616f071dea2764886
PrasantLakra <125748405+PrasantLakra@users.noreply.github.com>
2024-09-29 21:46:20 +0530

    Add files via upload

---
+++ IQR_Algo_Test/PowerVmAllocationPolicyAbstract.java
@@ -0,0 +1,125 @@
+/*
+ * Title:        CloudSim Toolkit
+ * Description:  CloudSim (Cloud Simulation) Toolkit for Modeling and Simulation of Clouds
+ * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
+ *
+ * Copyright (c) 2009-2012, The University of Melbourne, Australia
+ */
+
+package IQR_Algo_Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.VmAllocationPolicy;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.power.PowerHost;
+
+public abstract class PowerVmAllocationPolicyAbstract extends VmAllocationPolicy {
+
+	/** The vm table. */
+	private final Map<String, Host> vmTable = new HashMap<String, Host>();
+
+	/**
+	 * Instantiates a new power vm allocation policy abstract.
+	 * 
+	 * @param list the list
+	 */
+	public PowerVmAllocationPolicyAbstract(List<? extends Host> list) {
+		super(list);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.cloudbus.cloudsim.VmAllocationPolicy#allocateHostForVm(org.cloudbus.cloudsim.Vm)
+	 */
+	@Override
+	public boolean allocateHostForVm(Vm vm) {
+		return allocateHostForVm(vm, findHostForVm(vm));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.cloudbus.cloudsim.VmAllocationPolicy#allocateHostForVm(org.cloudbus.cloudsim.Vm,
+	 * org.cloudbus.cloudsim.Host)
+	 */
+	@Override
+	public boolean allocateHostForVm(Vm vm, Host host) {
+		if (host == null) {
+			Log.formatLine("%.2f: No suitable host found for VM #" + vm.getId() + "\n", CloudSim.clock());
+			return false;
+		}
+		if (host.vmCreate(vm)) { // if vm has been succesfully created in the host
+			getVmTable().put(vm.getUid(), host);
+			Log.formatLine(
+					"%.2f: VM #" + vm.getId() + " has been allocated to the host #" + host.getId(),
+					CloudSim.clock());
+			return true;
+		}
+		Log.formatLine(
+				"%.2f: Creation of VM #" + vm.getId() + " on the host #" + host.getId() + " failed\n",
+				CloudSim.clock());
+		return false;
+	}
+
+	/**
+	 * Find host for vm.
+	 * 
+	 * @param vm the vm
+	 * @return the power host
+	 */
+	
+	//IQR
+	public PowerHost findHostForVm(Vm vm) {
+		for (PowerHost host : this.<PowerHost> getHostList()) {
+			if (host.isSuitableForVm(vm)) {
+				return host;
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.cloudbus.cloudsim.VmAllocationPolicy#deallocateHostForVm(org.cloudbus.cloudsim.Vm)
+	 */
+	@Override
+	public void deallocateHostForVm(Vm vm) {
+		Host host = getVmTable().remove(vm.getUid());
+		if (host != null) {
+			host.vmDestroy(vm);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.cloudbus.cloudsim.VmAllocationPolicy#getHost(org.cloudbus.cloudsim.Vm)
+	 */
+	@Override
+	public Host getHost(Vm vm) {
+		return getVmTable().get(vm.getUid());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.cloudbus.cloudsim.VmAllocationPolicy#getHost(int, int)
+	 */
+	@Override
+	public Host getHost(int vmId, int userId) {
+		return getVmTable().get(Vm.getUid(userId, vmId));
+	}
+
+	/**
+	 * Gets the vm table.
+	 * 
+	 * @return the vm table
+	 */
+	public Map<String, Host> getVmTable() {
+		return vmTable;
+	}
+
+}
